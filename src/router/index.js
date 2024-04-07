98f48e5a716775a92ac2e00ea56758fceb0ad4f6
deanZhou <1182160314@qq.com>
2024-04-06 21:06:05 -0700

    router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,29 @@ import {
   createBrowserRouter,
 } from "react-router-dom";
 import { AuthRoute } from "@/components/AuthRoute";
-
+import Article from "@/pages/Article";
+import Publish from "@/pages/Publish";
+import Home from "@/pages/Home";
 //配置路由实例
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <AuthRoute><Layout /> </AuthRoute>
+    element: <AuthRoute><Layout /> </AuthRoute>,
+    children: [
+      {
+        index: true, // 将此路由设置为默认路由
+        element: <Home />
+      },
+      {
+        path: 'article',
+        element: <Article />
+      },
+      {
+        path: 'publish',
+        element: <Publish />
+      },
+
+    ]
   },
   {
     path: "/login",
